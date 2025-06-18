ba40ba4031a68f32788054759c22aa494254675a
kelly kilstein <kilstein.k@gmail.com>
2025-06-18 12:44:47 +0300

    Bumble bee after fixing CR issues + VS errors

---
+++ CppProjects/CppProjects/BumbleBee.cpp
@@ -0,0 +1,58 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+
+#include "GenericFunctions.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+/**
+ * @brief Initializes primes array of size arrayLen
+ * @param primesArray the array to insert primes to
+ * @param arrayLen the len of the array
+ */
+void initializePrimesArray(int* primesArray, unsigned int arrayLen) {
+    const int SMALLER_PRIME = 2;
+    unsigned int primesCounter = 0;
+    int currNum = SMALLER_PRIME;
+    bool foundInThisIteration = false;
+
+    while (primesCounter < arrayLen) {
+        while (!foundInThisIteration) {
+            bool isNumPrime = isPrime(currNum);
+
+            if (isNumPrime) {
+                foundInThisIteration = true;
+                primesArray[primesCounter] = currNum;
+            }
+
+            currNum++;
+        }
+
+        foundInThisIteration = false;
+        primesCounter++;
+    }
+}
+
+int main() {
+    unsigned int numOfPrimesToFind = 0;
+
+    try {
+        numOfPrimesToFind = getPositiveIntFromUser();
+    } catch (const std::exception& e) {
+        cout << e.what() << endl;
+        return 1;
+    }
+
+    int* primesArray = new int[numOfPrimesToFind];
+
+    initializePrimesArray(primesArray, numOfPrimesToFind);
+    printArray(primesArray, numOfPrimesToFind);
+
+    delete[] primesArray;
+
+    return 0;
+}
---
+++ CppProjects/CppProjects/GenericFunctions.cpp
@@ -0,0 +1,55 @@
+#include "GenericFunctions.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+void printArray(int* array, int arrayLen) {
+    cout << "The array: " << endl;
+
+    for (int i = 0; i < arrayLen; i++) {
+        cout << array[i] << " ";
+    }
+}
+
+bool isPrime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+
+    double userNumSqrt = sqrt(number);
+
+    for (int i = 2; i <= userNumSqrt; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int getPositiveIntFromUser() {
+    int userInputAsNum = 0;
+    string userInput = "";
+    size_t firstNotNumericIndex = 0;
+
+    cout << "Please enter a number: ";
+    cin >> userInput;
+
+    try {
+        userInputAsNum = stoi(userInput, &firstNotNumericIndex);
+    } catch (...) {
+        throw std::exception("Not a number! bye");
+    }
+
+    if (firstNotNumericIndex != userInput.length()) {
+        throw std::exception("Not a number! bye");
+    }
+
+    if (userInputAsNum < 1) {
+        throw std::exception("Not a positive number! bye");
+    }
+
+    return userInputAsNum;
+}
